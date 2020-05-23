05eae6f183961cf7c284c4e466786df8fb23a415
amazigh233 <45847460+amazigh233@users.noreply.github.com>
2020-05-23 02:59:00 +0200

    code smells eruit gehaald tot waar nodig was.
                                           JUNIT TEST GEMAAKT VOLGENS DE TESTPLANM

--- src/AttractiePark/CheckEntranceAttractiePark.java
+++ src/AttractiePark/CheckEntranceAttractiePark.java
@@ -4,17 +4,21 @@ import Bezoeker.BezoekerList;
 
 public class CheckEntranceAttractiePark {
 
-    private BezoekerList bezoekerList = new BezoekerList();
 
-    private AttractiePark attractiePark;
+    //primitive datatype
+    private int getMaxPersonen;
+    private int getTotaleBezoeker;
 
     public CheckEntranceAttractiePark() {
-        this.attractiePark = AttractiePark.getInstance();
+        AttractiePark attractiePark = AttractiePark.getInstance();
+        this.getMaxPersonen = attractiePark.getMaxPersonen();
+        BezoekerList bezoekerList = new BezoekerList();
+        this.getTotaleBezoeker =bezoekerList.getBezoekers().size();
     }
 
     public String checkIfVol(int aantal) {
         String msg ="Bezoeker kunnen attractiepark betreden";
-        if(this.bezoekerList.getBezoekers().size() + aantal >= this.attractiePark.getMaxPersonen()) {
+        if(this.getTotaleBezoeker + aantal >= this.getMaxPersonen) {
             msg = "vol, bezoekers kunnen attractie park niet betreden";
         }
         return msg;
